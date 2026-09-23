3b77f683c4223f634d455fec23db77e01010d93b
ichewinggum <santo2000x@gmail.com>
2026-09-22 23:08:18 -0700

    continued working on FoodEntry and User class

--- User.py
+++ User.py
@@ -7,16 +7,31 @@ class User:
         self.weight = weight
         self.activity = activity
 
-    log = []
+    book = {}
 
-    def add_food():
+    def add_food(self, foodEntry):
         pass
 
-    def remove_food():
-        pass
+    def remove_food(self, key, foodEntry):
+        if key in self.book:
+            del self.book[key]
+            print(f"Food entry '{key}' removed successfully.")
+        else:
+            print(f"Food entry '{key}' not found.")
+        
 
 
-    def edit_food():
-        pass
+    def edit_food(self, key, foodEntry):
+        if key in self.book:
+            self.book[key] = foodEntry
+            print(f"Food entry '{key}' updated successfully.")
+        else:
+            print(f"Food entry '{key}' not found.")
+        
 
- 
+  def view_book(self):
+        if not self.book:
+            print("No food entries found.")
+        else:
+            for key, foodEntry in self.book.items():
+                print(f"Key: {key}, Food: {foodEntry.food.name}, Amount Ate: {foodEntry.amount_ate}, Meal Type: {foodEntry.meal_type}, Date Ate: {foodEntry.date_ate}, Time Ate: {foodEntry.time_ate}")
